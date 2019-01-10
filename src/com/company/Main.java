d9f60c727274e9215bf06cd40564c8f3d9ba2965
Altynbek <altynnurbekovv2002@gmail.com>
2019-01-10 21:05:03 +0600

    Altynbek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,9 @@
 package com.company;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        System.out.println("Hello World");
     }
 }
